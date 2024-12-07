a34bbd608c7418c955d56d5e37921d770707ee4f
Curran Hill <currankhill@gmail.com>
2024-12-07 17:53:29 -0600

    Added hotword "computer", made speech recognition better by tweaking threshholds, and added threading that kinda does nothing atm but if I ever want a GUI its handy

--- Virtual Assistant POC/Virtual_Assistant_POC.py
+++ Virtual Assistant POC/Virtual_Assistant_POC.py
@@ -1,22 +1,44 @@
 import speech_recognition
 import pyttsx3
+import threading
 
-recognizer = speech_recognition.Recognizer() 
 
-while True:
+class Assistant:
     
-    try:
+    def __init__(self):
         
-        with speech_recognition.Microphone() as mic:
-            
-            recognizer.adjust_for_ambient_noise(mic, duration=0.2)
-            audio =recognizer.listen(mic)
-            
-            text = recognizer.recognize_google(audio)
-            text=text.lower()
-            
-            print(f"RECOGNIZED {text}")
+        self.recognizer = speech_recognition.Recognizer()
 
-    except speech_recognition.UnknownValueError:
-        recognizer = speech_recognition.Recognizer()
-        continue
+        threading.Thread(target=self.run_assistant).start()
+
+    def run_assistant(self):
+        while True:
+    
+            try:
+        
+                with speech_recognition.Microphone() as mic:
+            
+                    self.recognizer.dynamic_energy_threshold = True
+                    self.recognizer.adjust_for_ambient_noise(mic, duration=2)
+                    self.recognizer.pause_threshold = 2
+            
+                    audio = self.recognizer.listen(mic)
+            
+                    text = self.recognizer.recognize_google(audio)
+                    text=text.lower()
+            
+                    if "computer" in text:
+                        print(f"Go Ahead... \n")
+                        
+                        audio = self.recognizer.listen(mic)
+            
+                        text = self.recognizer.recognize_google(audio)
+                        text=text.lower()
+                
+                        print(f"#RECOGNIZED# {text} \n")
+
+            except speech_recognition.UnknownValueError:
+                self.recognizer = speech_recognition.Recognizer()
+                continue
+
+Assistant()
